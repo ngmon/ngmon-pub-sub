73283cfdbe047db9f439838d467684a50075e36c
xdanos <dan.tovarnak@gmail.com>
2012-10-31 14:10:09 +0100

    * now throwing exception for unsupported operator, embedded into Enum

--- src/main/java/cz/muni/fi/publishsubscribe/countingtree/Operator.java
+++ src/main/java/cz/muni/fi/publishsubscribe/countingtree/Operator.java
@@ -1,5 +1,34 @@
 package cz.muni.fi.publishsubscribe.countingtree;
 
+import java.util.*;
+
 public enum Operator {
-	EQUALS, LESS_THAN, GREATER_THAN, LESS_THAN_OR_EQUAL_TO, GREATER_THAN_OR_EQUAL_TO
+	EQUALS, LESS_THAN, LESS_THAN_OR_EQUAL_TO, GREATER_THAN, GREATER_THAN_OR_EQUAL_TO;
+
+	private static Map<Class, Set<Operator>> operatorMap = new HashMap<>(2);
+
+	static {
+		Set<Operator> stringOperators = new HashSet<>();
+		stringOperators.add(EQUALS);
+
+		Set<Operator> longOperators = new HashSet<>();
+		longOperators.add(EQUALS);
+		longOperators.add(LESS_THAN);
+		longOperators.add(LESS_THAN_OR_EQUAL_TO);
+		longOperators.add(GREATER_THAN);
+		longOperators.add(GREATER_THAN_OR_EQUAL_TO);
+
+		operatorMap.put(String.class, stringOperators);
+		operatorMap.put(Long.class, longOperators);
+
+	}
+
+	public static Set<Operator> getSupportedOperators(Class type) {
+
+		if (!operatorMap.containsKey(type)) {
+			return Collections.emptySet();
+		}
+
+		return operatorMap.get(type);
+	}
 }
