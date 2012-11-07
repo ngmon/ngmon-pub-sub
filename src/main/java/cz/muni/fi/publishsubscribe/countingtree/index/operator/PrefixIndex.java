a078c55528a27f0668cb3c89e0c7e2af7f49966d
Svata Novak <324881@mail.muni.cz>
2012-11-07 18:08:50 +0100

    Started implementing PrefixIndex - but TernarySearchTree needs Strings (CharSequence, to be exact), not Comparables as keys...

--- src/main/java/cz/muni/fi/publishsubscribe/countingtree/index/operator/PrefixIndex.java
+++ src/main/java/cz/muni/fi/publishsubscribe/countingtree/index/operator/PrefixIndex.java
@@ -1,29 +1,37 @@
 package cz.muni.fi.publishsubscribe.countingtree.index.operator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import cz.muni.fi.publishsubscribe.countingtree.Constraint;
+import cz.muni.fi.publishsubscribe.countingtree.ternarysearchtree.TernarySearchTree;
 
 public class PrefixIndex<T_ValueType extends Comparable<T_ValueType>>
 		implements OperatorIndex<T_ValueType> {
 
+	protected TernarySearchTree<Constraint> tree = new TernarySearchTree<>();
+
 	@Override
 	public boolean addConstraint(Constraint constraint) {
-		// TODO - implement
-		throw new UnsupportedOperationException("not yet implemented");
+		/*-
+		String value = constraint.getAttributeValue().getValue();
+		
+		return tree.put(value, constraint) != null;*/
+		
+		// DELETE THIS LATER!
+		return true;
 	}
 
 	@Override
 	public boolean removeConstraint(Constraint constraint) {
-		// TODO - implement
-		throw new UnsupportedOperationException("not yet implemented");
+		return (tree.remove(constraint.getAttributeValue().getValue()) != null);
 	}
 
 	@Override
 	public List<Constraint> getConstraints(Comparable<?> attributeValue) {
-		// TODO - implement
-		return new ArrayList<>();
+		/*-return tree.getAllPrefixes(attributeValue);*/
+		
+		// DELETE THIS LATER!
+		return null;
 	}
 
 }
