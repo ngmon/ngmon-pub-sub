33b0b8be17bb9d20227e68f7853039856662cca1
Svata Novak <324881@mail.muni.cz>
2012-11-11 18:47:02 +0100

    Finished String prefix index

--- src/main/java/cz/muni/fi/publishsubscribe/countingtree/index/type/StringIndex.java
+++ src/main/java/cz/muni/fi/publishsubscribe/countingtree/index/type/StringIndex.java
@@ -2,7 +2,7 @@ package cz.muni.fi.publishsubscribe.countingtree.index.type;
 
 import cz.muni.fi.publishsubscribe.countingtree.Operator;
 import cz.muni.fi.publishsubscribe.countingtree.index.operator.EqualsIndex;
-import cz.muni.fi.publishsubscribe.countingtree.index.operator.PrefixIndex;
+import cz.muni.fi.publishsubscribe.countingtree.index.operator.StringPrefixIndex;
 
 public class StringIndex extends AbstractTypeIndex<String> {
 
@@ -10,7 +10,7 @@ public class StringIndex extends AbstractTypeIndex<String> {
 		super();
 
 		this.addOperatorIndex(Operator.EQUALS, new EqualsIndex<String>());
-		this.addOperatorIndex(Operator.PREFIX, new PrefixIndex<String>());
+		this.addOperatorIndex(Operator.PREFIX, new StringPrefixIndex());
 	}
 
 
