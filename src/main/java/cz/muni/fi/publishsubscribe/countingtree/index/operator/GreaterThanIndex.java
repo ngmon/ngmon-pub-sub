89a3eea56c0316b47ad8964852fd7bc066cd8265
xdanos <dan.tovarnak@gmail.com>
2012-11-17 11:24:04 +0100

    * minor refactor

--- src/main/java/cz/muni/fi/publishsubscribe/countingtree/index/operator/GreaterThanIndex.java
+++ src/main/java/cz/muni/fi/publishsubscribe/countingtree/index/operator/GreaterThanIndex.java
@@ -5,10 +5,10 @@ import cz.muni.fi.publishsubscribe.countingtree.Constraint;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GreaterThanIndex<T_ValueType extends Comparable<T_ValueType>> extends AbstractNavigableMapIndex<T_ValueType> {
+public class GreaterThanIndex<T1 extends Comparable<T1>> extends AbstractNavigableMapIndex<T1> {
 
 	@Override
-	public List<Constraint<T_ValueType>> getConstraints(T_ValueType attributeValue) {
+	public List<Constraint<T1>> getConstraints(T1 attributeValue) {
 
 		return new ArrayList<>(this.constraints.headMap(attributeValue, false).values());
 	}
